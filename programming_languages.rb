f9faf918ca3fa222c8315eddfcc0537860bd0e2e
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-26 19:36:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |key, value|
+    new_hash[key] = value
+     new_hash[:ruby] = languages[:ruby]
+    new_hash[:style] = [:oo]
+  end
+  new_hash
 end
