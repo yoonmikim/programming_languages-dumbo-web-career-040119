d224ddd756c60678c313e6e648e412433da10e04
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-26 20:31:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,3 +10,20 @@ def reformat_languages(languages)
   end
   new_hash
 end
+
+def reformat_languages(languages)
+  # your code here
+  new_hash = {}
+  languages.each do |key, value|
+    value.each do |k, v|
+      new_hash[k] = v
+      new_hash[k][:style] = []
+      new_hash[k][:style] << key
+      if k == :javascript
+        new_hash[k][:style] = [:oo, :functional]
+      end
+    end
+  end
+  new_hash
+end
+puts reformat_languages(languages)
